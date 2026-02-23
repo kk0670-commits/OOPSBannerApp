8d1de4fa8eb9c90182ebb3d8ac92361565a91932
kk0670-commits <kk0670@srmist.edu.in>
2026-02-23 08:43:15 +0530

    Update OOPSBannerApp.java

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,23 +1,15 @@
-/**
- * OOPSBannerApp
- *
- * UC2: Render OOPS as Banner using Print Statements
- *
- * @author Sai Krishna
- * @version 2.0
- */
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        // Each letter is 7 lines height and width 9
-
-        System.out.println("  *****     *****    ******    ****** ");
-        System.out.println(" *     *   *     *   *     *   *      ");
-        System.out.println(" *     *   *     *   *     *   *      ");
-        System.out.println(" *     *   *     *   ******     ***** ");
-        System.out.println(" *     *   *     *   *          *     ");
-        System.out.println(" *     *   *     *   *          *     ");
-        System.out.println("  *****     *****    *          ******");
+        System.out.println(String.join(" ", "    ***   ", "    ***   ", "*****  ", "    *****  "));
+        System.out.println(String.join(" ", "  **   **", "   **   ** ", "**   **", "  **      "));
+        System.out.println(String.join(" ", " **     **", " **     **", "**   **", " **      "));
+        System.out.println(String.join(" ", " **     **", " **     **", "**   **", "  **      "));
+        System.out.println(String.join(" ", " **     **", " **     **", "*****  ", "    ****   "));
+        System.out.println(String.join(" ", " **     **", " **     **", "**     ", "        **  "));
+        System.out.println(String.join(" ", " **     **", " **     **", "**     ", "         **  "));
+        System.out.println(String.join(" ", "  **   **", "   **   ** ", "**     ", "        **  "));
+        System.out.println(String.join(" ", "    ***   ", "    ***   ", "**     ", "   *****   "));
     }
 }
